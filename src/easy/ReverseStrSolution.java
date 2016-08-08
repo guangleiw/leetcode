8935d415a7dd8ffcdaffb2a2994f530aafadc712
wangguanglei <wangguanglei1@jd.com>
2016-08-08 15:26:09 +0800

    Using StringBuilder to test the solution

--- src/easy/ReverseStrSolution.java
+++ src/easy/ReverseStrSolution.java
@@ -1,17 +1,59 @@
 package easy;
+
 public class ReverseStrSolution {
-    public String reverseString(String s) {   	
-    	
-        StringBuffer sb = new StringBuffer(s);
-        for (int i = 0; i < sb.length()/2; i++) {
-        	int swap_index = sb.length() - i -1;
-        	if(sb.charAt(i) == sb.charAt(swap_index)){
-        		continue;
-        	}			
-        	char temp = sb.charAt(i);
-        	sb.setCharAt(i, sb.charAt(swap_index));
-        	sb.setCharAt(swap_index, temp);
+	public String reverseString(String s) {
+
+		long startTime=System.nanoTime();   //获取开始时间  	
+		
+		StringBuffer sb = new StringBuffer(s);
+		for (int i = 0; i < sb.length() / 2; i++) {
+			int swap_index = sb.length() - i - 1;
+			if (sb.charAt(i) == sb.charAt(swap_index)) {
+				continue;
+			}
+			char temp = sb.charAt(i);
+			sb.setCharAt(i, sb.charAt(swap_index));
+			sb.setCharAt(swap_index, temp);
 		}
-        return sb.toString();
-    }
+		
+		long endTime=System.nanoTime(); //获取结束时间  
+		System.out.println("程序运行时间： "+(endTime-startTime)+"ns");   	
+		
+		return sb.toString();
+	}
+	
+	public String reverseStringWithStringBuilder(String s){
+		long startTime = System.nanoTime();
+		
+		StringBuilder sb = new StringBuilder(s);
+		for (int i = 0; i < sb.length()/2; i++) {
+			int tail = sb.length() -i-1;
+			if(sb.charAt(i) == sb.charAt(tail)){
+				continue;
+			}
+			char temp  = sb.charAt(i);
+			sb.setCharAt(i, sb.charAt(tail));
+			sb.setCharAt(tail, temp);
+		}
+		long endTime = System.nanoTime();
+		System.out.println("程序运行时间： "+(endTime-startTime)+"ns");
+		
+		return sb.toString();
+	}
+	
 }
+
+
+/**
+long startTime=System.nanoTime();   //获取开始时间  
+doSomeThing();  //测试的代码段  
+long endTime=System.nanoTime(); //获取结束时间  
+System.out.println("程序运行时间： "+(endTime-startTime)+"ns");   
+**/
+
+/**
+long startTime=System.currentTimeMillis();   //获取开始时间  
+doSomeThing();  //测试的代码段  
+long endTime=System.currentTimeMillis(); //获取结束时间  
+System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
+**/
--- src/easy/Solution.java
+++ src/easy/Solution.java
@@ -5,7 +5,9 @@ public class Solution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ReverseStrSolution rs = new ReverseStrSolution();
-		System.out.println(rs.reverseString(new String("")));
+		System.out.println(rs.reverseString(new String("This is a good example for reseving String in JAVA")));
+		System.out.println(
+				rs.reverseStringWithStringBuilder((new String("This is a good example for reseving String in JAVA"))));
 	}
 
 }
