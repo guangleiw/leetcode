18965ca0a08c2f100b8430f44177534562f62bab
wangguanglei <wangguanglei1@jd.com>
2016-09-07 10:36:50 +0800

    add solution for count and say

---
+++ src/easy/CountSaySolution.java
@@ -0,0 +1,32 @@
+package easy;
+
+public class CountSaySolution {
+	public String countAndSay(int n) {
+		String s = new String("1");
+		if (n <= 0)
+			return null;
+		if(1== n) return s;
+		String preStr = s;
+		StringBuffer newStrBuf = new StringBuffer("");
+		for (int c = 1; c < n; c++) {
+			int count = 1;
+			char value = '1';
+			newStrBuf = new StringBuffer("");
+			for (int i = 0, j = 1; j < preStr.length(); j++) {
+				value = preStr.charAt(i);
+				if (preStr.charAt(i) == preStr.charAt(j)) {
+					count++;
+				} else {
+					newStrBuf.append(count);
+					newStrBuf.append(preStr.charAt(i));
+					i = j;
+					count = 1;
+				}
+			}
+			newStrBuf.append(count);
+			newStrBuf.append(preStr.charAt(preStr.length()-1));
+			preStr = newStrBuf.toString();
+		}
+		return newStrBuf.toString();
+	}
+}
