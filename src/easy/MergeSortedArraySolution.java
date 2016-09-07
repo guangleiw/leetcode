91e506a884e5ac2f1ea45bae344a52b22a9aea17
guangleiw <stylite.wang@gmail.com>
2016-09-07 14:26:09 +0800

    Bug fix for solution

--- src/easy/MergeSortedArraySolution.java
+++ src/easy/MergeSortedArraySolution.java
@@ -2,26 +2,23 @@ package easy;
 
 public class MergeSortedArraySolution {
 	public void merge(int[] nums1, int m, int[] nums2, int n) {
-		if (m == 0){
+		if (m == 0) {
 			System.arraycopy(nums2, 0, nums1, 0, n);
 			return;
 		}
-		if(n ==0){
+		if (n == 0) {
 			return;
 		}
-		
+
 		int idx_n = n - 1;
 		int idx_m = m - 1;
-		int fill_idx = m+n - 1;
+		int fill_idx = m + n - 1;
 		while (idx_n >= 0 && idx_m >= 0) {
 			if (nums1[idx_m] >= nums2[idx_n]) {
-				nums1[fill_idx] = nums1[idx_m];
-				idx_m--;
+				nums1[fill_idx--] = nums1[idx_m--];
 			} else {
-				nums1[fill_idx] = nums2[idx_n];
-				idx_n--;
+				nums1[fill_idx--] = nums2[idx_n--];
 			}
-			fill_idx--;
 		}
 		while (idx_m >= 0) {
 			nums1[fill_idx--] = nums1[idx_m--];
