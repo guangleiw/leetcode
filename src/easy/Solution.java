6aee49ceccb9bf7d8b0f9d3c5f8249f7c67bb441
guangleiw <stylite.wang@gmail.com>
2016-09-12 10:39:02 +0800

    Add test case for ContainsDuplicateSolution

--- src/easy/Solution.java
+++ src/easy/Solution.java
@@ -168,11 +168,14 @@ public class Solution {
 		 * MergeSortedArraySolution msas = new MergeSortedArraySolution(); int[]
 		 * a = new int[]{0}; int[] b = new int[]{1}; a = new int[]{1}; b = new
 		 * int[]{}; msas.merge(a, 1, b, 0); printArray(a);
+		 * 
+		 * 
+		 * ValidParenthessSolution vps = new ValidParenthessSolution();
+		 * System.out.println(vps.isValid("([)]"));
 		 **/
 		
-		ValidParenthessSolution vps = new ValidParenthessSolution();
-		System.out.println(vps.isValid("([)]"));
-
+		ContainsDuplicateSolution cds = new ContainsDuplicateSolution();
+		System.out.println(cds.containsNearbyDuplicate(new int[]{1,2}, 2));
 	}
 
 	public static void printArray(int[] arr) {
