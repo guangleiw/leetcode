029a4ba2d154a04bc390cb1bb47335e8573bf434
guangleiw <stylite.wang@gmail.com>
2016-09-13 11:17:46 +0800

    Add remove linked list elements solution

---
+++ src/easy/RemoveLinkedListElementsSolution.java
@@ -0,0 +1,30 @@
+package easy;
+
+import utils.ListNode;
+
+public class RemoveLinkedListElementsSolution {
+	public ListNode removeElements(ListNode head, int val) {
+		if (head == null)
+			return head;
+		if (head.next == null) {
+			if (head.val == val)
+				return null;
+			else {
+				return head;
+			}
+		}
+
+		ListNode f = head.next;
+		ListNode s = head;
+		while (f != null) {
+			if (f.val == val) {
+				f = f.next;
+				s.next = f;
+			} else {
+				f = f.next;
+				s = s.next;
+			}
+		}
+		return head;
+	}
+}
