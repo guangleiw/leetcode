de532ffa30c8a7aa34cf812d9abd09c3980634f4
guangleiw <stylite.wang@gmail.com>
2016-09-18 10:26:42 +0800

    Add solution for reversing bits

--- src/easy/ReverseBitsSolution.java
+++ src/easy/ReverseBitsSolution.java
@@ -4,11 +4,14 @@ public class ReverseBitsSolution {
 	public int reverseBits(int n) {
 		if (0 == n || 0xFFFFFFFF == n)
 			return n;
-		int a = ((n&0xAAAAAAAA) >>1)|((n&0x55555555) << 1);
-		a = ((a&0xCCCCCCCC) >>2)|((a&0x33333333) << 2);
-		 a = ((a&0xF0F0F0F0) >>4)|((a&0x0F0F0F0F) << 4);
-		 a = ((a&0xFF00FF00) >>8)|((a&0x00FF00FF) << 8);
-		 a = ((a&0xFFFF0000) >>16)|((a&0x0000FFFF) << 16);
-		return a;
+		int b = 0;
+		int nums = 32;
+		while (nums != 0) {
+			b = b << 1;
+			b = b | (n & 0x01);
+			n = n >> 1;
+			nums--;
+		}
+		return b;
 	}
 }
